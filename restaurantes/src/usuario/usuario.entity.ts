1f0cef0d5920c2d4b5f0674cddcdb91aaa03adce
washitos <55854457+washitos@users.noreply.github.com>
2020-02-02 23:58:30 -0500

    Tabla Usuario
    Creacion de la tabla usuario, faltan validaciones

---
+++ restaurantes/src/usuario/usuario.entity.ts
@@ -0,0 +1,101 @@
+import {Column, Entity, Index, OneToMany, PrimaryGeneratedColumn} from 'typeorm';
+
+@Entity('usuario_web')
+export class UsuarioEntity {
+    @PrimaryGeneratedColumn({
+        type: 'int',
+        unsigned: true,
+        name: 'id_web',
+        comment: 'Identificador de la tabla usuario',
+    })
+    id: number;
+
+    @Index({
+        unique: false,
+    })
+    @Column({
+        type: 'varchar',
+        nullable: true,
+        name: 'nombre',
+        comment: 'Nombre de la tabla usuario',
+    })
+    nombre?: string;
+
+    @Index({
+        unique: false,
+    })
+    @Column({
+        type: 'varchar',
+        nullable: true,
+        name: 'apellido',
+        comment: 'Apellido de la tabla usuario',
+    })
+    apellido?: string;
+
+    @Index({
+        unique: false,
+    })
+    @Column({
+        type: 'varchar',
+        nullable: true,
+        name: 'contrasena',
+        comment: 'Nombre de la tabla usuario',
+    })
+    contrasena?: string;
+
+    @Index({
+        unique: false,
+    })
+    @Column({
+        type: 'varchar',
+        nullable: true,
+        name: 'fechaNacimiento',
+        comment: 'Fecha de Nacimiento usuario',
+    })
+    fechaNacimiento?: string;
+
+    @Index({
+        unique: false,
+    })
+    @Column({
+        type: 'varchar',
+        nullable: true,
+        name: 'correoElectronico',
+        comment: 'Correo Electronico de la tabla usuario',
+    })
+    correoElectronico?: string;
+
+    @Index({
+        unique: true,
+    })
+    @Column({
+        type: 'varchar',
+        nullable: false,
+        name: 'cedula',
+        comment: 'Cedula de la tabla usuario',
+    })
+    cedula: string;
+
+    @Index({
+        unique: true,
+    })
+    @Column({
+        type: 'varchar',
+        nullable: false,
+        name: 'direccionDomicilio',
+        comment: 'Direccion de domicilio de la tabla usuario',
+    })
+    direccionDomicilio: string;
+
+    @Index({
+        unique: true,
+    })
+    @Column({
+        type: 'varchar',
+        nullable: false,
+        name: 'codigoPostal',
+        comment: 'Codigo Postal de la tabla usuario',
+    })
+    codigoPostal: string;
+
+}
---
+++ restaurantes/src/usuario/usuario.service.ts
@@ -0,0 +1,16 @@
+import {Injectable} from "@nestjs/common";
+import {InjectRepository} from "@nestjs/typeorm";
+import {UsuarioEntity} from "./usuario.entity";
+import {DeleteResult, Like, MoreThan, Repository} from "typeorm";
+
+@Injectable()
+export class UsuarioService {
+    constructor(
+        @InjectRepository(UsuarioEntity) // Inyectar Dependencias
+        private _repositorioUsuario: Repository<UsuarioEntity>
+    ) {
+    }
+
+
+
+}
